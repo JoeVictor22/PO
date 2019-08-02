cd558032bea81ee61116bcf3d9497cd286a96259
Joel <JoelVIctor1746@gmail.com>
2019-08-02 18:14:00 -0300

    Delete insertion_sort.py

--- Ordenacao/Insertion/insertion_sort.py
+++
@@ -1,9 +0,0 @@
-
-def insertion(lista):
-  for x in range(0, len(lista)):
-    key = lista[x]
-    y = x-1
-    while key < lista[y] and y>=0:
-      lista[y+1] = lista[y]
-      y-=1
-    lista[y+1] = key
