25f5810ecb41d732026b3d9df7195865f7e3a6d2
Joel <JoelVIctor1746@gmail.com>
2019-08-15 11:45:21 -0300

    Update merge.py

--- Ordenacao/Merge/merge.py
+++ Ordenacao/Merge/merge.py
@@ -1,4 +1,26 @@
-def sort(lista): 
+import timeit
+from random import randint, shuffle
+import matplotlib.pyplot as plt
+
+def geraLista(tam):
+    lista = list(range(1, tam + 1))
+    shuffle(lista)
+    return lista
+      
+
+def desenhaGrafico(x,lista1,xl = "Entradas", yl = "Y",name="out", label1 = "Lista 1", label2 = "Lista 2"):
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
+    ax.plot(x, lista1, label = label1)
+    ax.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
+    plt.ylabel(yl)
+    plt.xlabel(xl)
+    plt.savefig(name)
+
+def sort(lista):
+    merge(lista)
+
+def merge(lista): 
     if len(lista) >1: 
         meio = int(len(lista)/2)
         esquerda = [] 
@@ -8,8 +30,8 @@ def sort(lista):
         for i in range(meio, len(lista)):
             direita.append(lista[i])
   
-        mergeSort(esquerda) 
-        mergeSort(direita) 
+        merge(esquerda) 
+        merge(direita) 
   
         i = j = k = 0     
         while i < len(esquerda) and j < len(direita): 
@@ -30,3 +52,14 @@ def sort(lista):
             lista[k] = direita[j] 
             j+=1
             k+=1
+
+quant = [100000,200000,400000,500000,1000000,2000000]
+graf_tempo = []
+
+for i in range(len(quant)):
+    print(quant[i])
+    lista = geraLista(quant[i])
+    graf_tempo.append(timeit.timeit("sort({})".format(lista),setup="from __main__ import sort",number=1))
+
+
+desenhaGrafico(quant,graf_tempo,"Tamanho", "Tempo", "saida_time", label1 = "Lista")
