ca165e5f47ac7eb0dae96ece708e1ebb03f97f3d
Joel <JoelVIctor1746@gmail.com>
2019-11-01 10:26:18 -0300

    Update patricia.py

--- Patricia/patricia.py
+++ Patricia/patricia.py
@@ -7,12 +7,16 @@ class patricia():
         i = 0
         while 1:
             try:
+				#verifica se existe algo no node
                 node = data[word[i:i+1]]
             except KeyError:	
                 if data:
+					#cria a key como indexação do node
                     data[word[i:i+1]] = [word[i+1:],{}]
                 else:
+					#se nao existir node
                     if word[i:i+1] == '':
+						#se a palavra ja existir
                         return
                     else:
                         if i != 0:
@@ -20,19 +24,26 @@ class patricia():
                         data[word[i:i+1]] = [word[i+1:],{}]
                 return
 
+			#existindo algo no node, verifica primeiro caractere da palavra de acordo com o index
             i += 1
             if word.startswith(node[0],i):
+				#se o resto da palavra apos o index é identico ao valor dentro do node
+				#registra um node informando q é uma palavra
                 if len(word[i:]) == len(node[0]):
                     if node[1]:
+						#se existe nos
+						#cria um item informando q é uma palavra
                         try:
                             node[1]['']
                         except KeyError:
                             data = node[1]
                             data[''] = ['',{}]
                     return
+				#acessa o proximo node
                 else:
                     i += len(node[0])
                     data = node[1]
+			#acessa os nos gardados nesse nivel
             else:
                 ii = i
                 j = 0
@@ -45,10 +56,21 @@ class patricia():
                 tmpdata[word[ii:ii+1]] = [word[ii+1:],{}]
                 data[word[i-1:i]] = [node[0][:j],tmpdata]
                 return
+   
+    def addText(self, text):
+    	#text = text.strip([',', '.'])
+    	text = text.replace(',', '')
+    	text = text.replace('.', '')
+    	text = text.split(' ')
+
+    	for word in text:
+    		self.add(word)
+
 
     def isWord(self,word):
         data = self._data
         i = 0
+		# acessa node por node em busca da palavra
         while 1:
             try:
                 node = data[word[i:i+1]]
@@ -73,6 +95,7 @@ class patricia():
         data = self._data
         i = 0
         wordlen = len(word)
+		#verifica o inicio da palavra com a key e o valor de cada node
         while 1:
             try:
                 node = data[word[i:i+1]]
@@ -92,12 +115,14 @@ class patricia():
         data = self._data
         i = 0
         while 1:
+        	#checa se o node existe
             try:
                 node = data[word[i:i+1]]
             except KeyError:
                 print( "Não está presente")
                 return
             i += 1
+            #busca o node e o remove
             if word.startswith(node[0],i):
                 if len(word[i:]) == len(node[0]):
                     if node[1]:
@@ -117,22 +142,22 @@ class patricia():
                 return
 
 
-    __getitem__ = isWord	
+	
 
 
 def main():
 
 	x = patricia()
-	x.add('bacana')
+	x.add('baiacu')
 	print(x._data)
 
 	x.add('bacalhau')
 	print(x._data)
 
-	x.add('baca')
+	x.add('bagre')
 	print(x._data)
 
-	print(x.isPrefix('ba'))
+	print(x.isPrefix('bac'))
 
 
 if __name__ == "__main__":
